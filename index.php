7aa8bc41efd2a1322f35eb93c7d56eb09129598a
faricandrevano <faric26andrevano@gmail.com>
2024-06-07 21:08:33 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Homepage</title>
+</head>
+<body>
+
+<?php 
+	echo "Halo Bunnies, selamat datang!
+";
+ ?>
+
+<h3><a href="../JeansWeb/index.html">JeansWeb</a></h3>
+
+</body>
+</html>
